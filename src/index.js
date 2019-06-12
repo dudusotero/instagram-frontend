3e3817d53433a2fe52c9f069ded0e690075a8f2b
Eduardo Sotero <eduardo@feracode.com>
2019-06-12 13:25:09 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./global.css";
+import Header from "./components/Header";
+import Routes from "./routes";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes />
+  </BrowserRouter>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
